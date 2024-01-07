83b9cdb4ab8443aeceb85a9274fc8c8aa562f1b0
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2024-01-07 15:25:34 +0530

    main form creation board

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-//import NextTopLoader from "nextjs-toploader";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {/*<NextTopLoader />*/}
+          <NextTopLoader />
           <DesignerContextProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
               {children}
